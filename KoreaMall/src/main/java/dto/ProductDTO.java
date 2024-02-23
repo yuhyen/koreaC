6e775561ddec4e8f6ef30c4b2eda3e5c55892c9d
jiyun9 <yje303@naver.com>
2024-02-23 17:49:55 +0900

    productdto 생성

---
+++ KoreaMall/src/main/java/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+	
+	private String p_name;
+	private String p_explain;
+	private String p_size;
+	private String p_color;
+	private String p_pabric;
+	private String p_picture;
+	
+	private int p_num;
+	private int p_price;
+	private int p_id;
+}
