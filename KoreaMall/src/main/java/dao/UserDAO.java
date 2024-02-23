382e93527904ad2cf9aafbee4d8fcfa1644f0e2b
yuhyen <yuhyen98@nate.com>
2024-02-23 15:57:13 +0900

    KoreaMall 프로젝트 메인에 올리기 2월 23일 3시 57분

---
+++ KoreaMall/src/main/java/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserDAO {
+	final SqlSession sqlSession;
+	
+	public List<UserDTO> selectList(){
+		return sqlSession.selectList("user.user_list");
+	}
+}
---
+++ KoreaMall/src/main/java/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private int u_idx;
+	private String u_id,u_pwd,u_username,u_email,u_tel,u_grade;
+}
