dbe43e19f5915af8201affc100939e9cd691b243
jiyun9 <yje303@naver.com>
2024-03-12 16:18:06 +0900

    fix 상수경로변

--- src/main/java/com/korea/mall/common/Const.java
+++ src/main/java/com/korea/mall/common/Const.java
@@ -2,7 +2,7 @@ package com.korea.mall.common;
 
 public class Const {
 
-	public static final String FILE_PATH = "/resources/file/";
+	public static final String FILE_PATH = "/resources/product_img/";
 	public static final String U_SESSION_KEY = "u_id";
 	
 }
